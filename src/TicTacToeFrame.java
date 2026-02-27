c23f397a9f90cde70f6cd07f2c45f06b900e39c8
soph <sophia@javamate.net>
2026-02-27 13:58:05 -0500

    Commit

--- src/TicTacToeFrame.java
+++
@@ -1,5 +0,0 @@
-public class TicTacToeFrame {
-    public static void main(String[] args) {
-
-    }
-}
--- src/TicTacToeRunner.java
+++ src/TicTacToeRunner.java
@@ -1,2 +1,5 @@
 public class TicTacToeRunner {
+    public static void main(String[] args) {
+        TicTacToeFrame test = new TicTacToeFrame();
+    }
 }
